66732af4f91e26b4343cb472fda89900cf9fb858
Youngjun Choi <cyoungjuene@gmail.com>
2023-08-01 16:33:15 +0900

    add pretrained ckpt

--- mvd/generate.py
+++ mvd/generate.py
@@ -1,48 +1,86 @@
+import pdb
 import os
+import glob
 import re
 import argparse
-from glob import glob
 import torch
+import torchaudio as T
+import torchaudio.transforms as TT
 import librosa
 import soundfile as sf
 import numpy as np
+from params import params
 from pythonosc import udp_client
 from inference import predict
 
 
-def select_checkpoint(ckpt_dir, epoch):
-    checkpoint_files = glob.glob(f'./{ckpt_dir}/weights-*.pt')
+def audio_to_mel(audio, sr):
+    if params.sample_rate != sr:
+        raise ValueError(f'Invalid sample rate {sr}.')
+    mel_args = {
+        'sample_rate': sr,
+        'win_length': params.hop_samples * 4,
+        'hop_length': params.hop_samples,
+        'n_fft': params.n_fft,
+        'f_min': 20.0,
+        'f_max': sr / 2.0,
+        'n_mels': params.n_mels,
+        'power': 1.0,
+        'normalized': True,
+    }
+    mel_spec_transform = TT.MelSpectrogram(**mel_args)
+    mel_spectrogram = mel_spec_transform(audio)
+    return mel_spectrogram
 
-    if epoch == -1:
-        return os.path.join(ckpt_dir, 'weights.pt')
+
+def select_checkpoint(args):
+    if args.ckpt_path is not None:
+        return args.ckpt_path
+    
+    checkpoint_files = glob.glob(f'./{args.ckpt_dir}/weights-*.pt')
+    if args.epoch == -1:
+        return os.path.join(args.ckpt_dir, 'weights.pt')
     else:
         pattern = re.compile(r'weights-(\d+).pt')
         steps = [int(pattern.match(os.path.basename(f)).group(1)) for f in checkpoint_files]
         steps_per_epoch = steps[1]
-        target_steps = steps_per_epoch * epoch
-        ckpt_file = f'./{ckpt_dir}/weights-{target_steps}.pt'
+        target_steps = steps_per_epoch * args.epoch
+        ckpt_file = f'./{args.ckpt_dir}/weights-{target_steps}.pt'
         if ckpt_file in checkpoint_files:
             return ckpt_file
         else:
-            raise ValueError(f'No checkpoint found for {epoch} epochs.')
+            raise ValueError(f'No checkpoint found for {args.epoch} epochs.')
         
 
-def main(args, input_audio):
-    spec = librosa.feature.melspectrogram(input_audio, sr=22050, n_fft=1024, hop_length=256, n_mels=80)
-    spec = torch.from_numpy(np.load(spec))
-    ckpt = select_checkpoint(args.ckpt_dir, args.epoch)
-    recon_audio, sample_rate = predict(spec, ckpt, fast_sampling=True)
-    y = recon_audio.detach().cpu().numpy()
-    sf.write(args.save_file, y, sample_rate)
-    return y
+def main(args):
+    audio, sr = T.load(args.wav_input)
+    audio = torch.clamp(audio[0], -1.0, 1.0)
+
+    with torch.no_grad():
+        mel = audio_to_mel(audio, sr)
+        
+    ckpt = select_checkpoint(args)
+    recon_audio, sample_rate = predict(mel, ckpt, fast_sampling=True)
+    recon_audio = recon_audio.detach().cpu().numpy()
+    recon_audio = recon_audio.flatten()
+    audio = audio.detach().cpu().numpy()
+
+    if recon_audio.size > audio.size:
+        recon_audio = recon_audio[:audio.size]
+    elif recon_audio.size < audio.size:
+        audio = audio[:recon_audio.size]
+    added_audio = audio + recon_audio
+    sf.write(args.wav_output, added_audio, sample_rate)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Select a checkpoint epoch.')
+    parser.add_argument('--ckpt_path', type=str, default=None,
+                        help='Path to the checkpoint file.')
     parser.add_argument('--ckpt_dir', type=str, default='./ckpt/',
                         help='Path to the checkpoint directory.')
     parser.add_argument('-i', '--wav_input', type=str, default='input.wav')
-    parser.add_argument('-o', '--wav_output', type=str, default='recon.wav')
+    parser.add_argument('-o', '--wav_output', type=str, default='output.wav')
     parser.add_argument('-e', '--epoch', type=int, default=-1,
                         help='Epoch of the checkpoint to select. Set to -1 to select the best checkpoint.')
     parser.add_argument("--ip", default="127.0.0.1", help="The ip of the OSC server")
@@ -51,7 +89,6 @@ if __name__ == '__main__':
 
     client = udp_client.SimpleUDPClient(args.ip, args.port)
     
-    main(args, args.wav_input)
+    main(args)
 
     client.send_message("/audio", args.wav_output)
-
