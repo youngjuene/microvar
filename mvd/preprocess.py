2aace956e81295a53f5d9e2f3701cba7355d6c58
Youngjun Choi <cyoungjuene@gmail.com>
2023-08-01 13:10:38 +0900

    preprocessing

--- mvd/preprocess.py
+++ mvd/preprocess.py
@@ -1,8 +1,10 @@
-import numpy as np
 import torch
 import torchaudio as T
 import torchaudio.transforms as TT
 import librosa
+import soundfile as sf
+import numpy as np
+import os
 
 from argparse import ArgumentParser
 from concurrent.futures import ProcessPoolExecutor
@@ -11,31 +13,36 @@ from tqdm import tqdm
 
 from params import params
 
+def create_directories(output_dir, option):
+    if option == 'hpss':
+        os.makedirs(os.path.join(output_dir, 'harm'), exist_ok=True)
+        os.makedirs(os.path.join(output_dir, 'perc'), exist_ok=True)
+    else: # option == 'cen'
+        os.makedirs(os.path.join(output_dir, 'target'), exist_ok=True)
+        os.makedirs(os.path.join(output_dir, 'residue'), exist_ok=True)
 
-def preprocess_audio(y, sr, option, pad):
-    # STFT
-    D = librosa.stft(y, n_fft=params.n_fft, hop_length=params.hop_samples, win_length=params.hop_samples * 4)
-
+def separate_spectrogram(D, sr, option, pad):
     if option == 'hpss':
         # HPSS
-        H, P = librosa.decompose.hpss(D)
-        return H, P
+        mel_target, mel_residue = librosa.decompose.hpss(D)
+
     elif option == 'cen':
         # Spectral centroid
-        spectral_centroids = librosa.feature.spectral_centroid(y, sr=sr)[0]
+        spectral_centroids = librosa.feature.spectral_centroid(S=np.abs(D), sr=sr)[0]
 
-        centroid_max = min(spectral_centroids.argmax() + pad, len(spectral_centroids) - 1)
+        centroid_max = min(spectral_centroids.argmax() + pad, D.shape[0] - 1)
         centroid_min = max(spectral_centroids.argmin() - pad, 0)
 
         mask = np.zeros(D.shape, dtype=bool)
         mask[centroid_min:centroid_max, :] = True
 
-        target = D * mask
-        residue = D * ~mask
+        mel_target = D * mask
+        mel_residue = D * ~mask
 
-        return target, residue
     else:
         raise ValueError('Option must be "hpss" or "cen".')
+
+    return mel_target, mel_residue
 
 def transform(filename, option, pad):
     audio, sr = T.load(filename)
@@ -43,38 +50,78 @@ def transform(filename, option, pad):
 
     if params.sample_rate != sr:
         raise ValueError(f'Invalid sample rate {sr}.')
+    mel_args = {
+        'sample_rate': sr,
+        'win_length': params.hop_samples * 4,
+        'hop_length': params.hop_samples,
+        'n_fft': params.n_fft,
+        'f_min': 20.0,
+        'f_max': sr / 2.0,
+        'n_mels': params.n_mels,
+        'power': 1.0,
+        'normalized': True,
+    }
+    mel_spec_transform = TT.MelSpectrogram(**mel_args)
 
     with torch.no_grad():
-        # Apply the preprocessing
-        audio1, audio2 = preprocess_audio(audio.numpy(), sr, option, pad)
-        audio1 = torch.from_numpy(audio1).float()
-        audio2 = torch.from_numpy(audio2).float()
+        # Convert the audio to a mel spectrogram
+        mel_spectrogram = mel_spec_transform(audio)
 
-        mel_spec_transform = TT.MelSpectrogram(sample_rate=sr, win_length=params.hop_samples * 4, hop_length=params.hop_samples, n_fft=params.n_fft, f_min=20.0, f_max=sr / 2.0, n_mels=params.n_mels, power=1.0, normalized=True)
-        spectrogram1 = mel_spec_transform(audio1)
-        spectrogram2 = mel_spec_transform(audio2)
+        # Convert the mel spectrogram back to a linear-frequency spectrogram
+        D = librosa.feature.inverse.mel_to_stft(mel_spectrogram.numpy(), sr=sr, n_fft=params.n_fft, power=1.0)
 
-        spectrogram1 = 20 * torch.log10(torch.clamp(spectrogram1, min=1e-5)) - 20
-        spectrogram1 = torch.clamp((spectrogram1 + 100) / 100, 0.0, 1.0)
-        spectrogram2 = 20 * torch.log10(torch.clamp(spectrogram2, min=1e-5)) - 20
-        spectrogram2 = torch.clamp((spectrogram2 + 100) / 100, 0.0, 1.0)
+        # Apply the chosen source separation method
+        D_target, D_residue = separate_spectrogram(D, sr, option, pad)
 
-        np.save(f'{filename}_part1.spec.npy', spectrogram1.cpu().numpy())
-        np.save(f'{filename}_part2.spec.npy', spectrogram2.cpu().numpy())
+        # Convert the separated sources back to mel spectrograms
+        mel_target = mel_spec_transform(torch.from_numpy(librosa.istft(D_target)).float())
+        mel_residue = mel_spec_transform(torch.from_numpy(librosa.istft(D_residue)).float())
 
+        # Apply the log-amplitude transformation and scale the spectrograms
+        mel_target = 20 * torch.log10(torch.clamp(mel_target, min=1e-5)) - 20
+        mel_target = torch.clamp((mel_target + 100) / 100, 0.0, 1.0)
+        mel_residue = 20 * torch.log10(torch.clamp(mel_residue, min=1e-5)) - 20
+        mel_residue = torch.clamp((mel_residue + 100) / 100, 0.0, 1.0)
+
+        # Save the separated spectrograms
+        output_dir = f'../data/{args.audio_dir}'
+        create_directories(output_dir, option)
+
+        if option == 'hpss':
+            np.save(f"{output_dir}/harm/{filename.split('/')[-1].replace('.wav', '.npy')}", mel_target.cpu().numpy())
+            np.save(f"{output_dir}/perc/{filename.split('/')[-1].replace('.wav', '.npy')}", mel_residue.cpu().numpy())
+            sf.write(f"{output_dir}/harm/{filename.split('/')[-1]}", librosa.istft(D_target), sr)
+            sf.write(f"{output_dir}/perc/{filename.split('/')[-1]}", librosa.istft(D_residue), sr)
+        else: # option == 'cen'
+            np.save(f"{output_dir}/target/{filename.split('/')[-1].replace('.wav', '.npy')}", mel_target.cpu().numpy())
+            np.save(f"{output_dir}/residue/{filename.split('/')[-1].replace('.wav', '.npy')}", mel_residue.cpu().numpy())
+            sf.write(f"{output_dir}/target/{filename.split('/')[-1]}", librosa.istft(D_target), sr)
+            sf.write(f"{output_dir}/residue/{filename.split('/')[-1]}", librosa.istft(D_residue), sr)
+            
 
 def main(args):
-    filenames = glob(f'{args.dir}/**/*.wav', recursive=True)
-    with ProcessPoolExecutor() as executor:
-        list(tqdm(executor.map(lambda filename: transform(filename, args.sep, args.pad), filenames), desc='Preprocessing', total=len(filenames)))
+    filenames = glob(f'../data/{args.audio_dir}/*.wav')
+    for filename in tqdm(filenames, desc='Preprocessing', total=len(filenames)):
+        transform(filename, args.sep, args.pad)
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Prepares a dataset to train DiffWave')
-    parser.add_argument('--dir',
+    parser = ArgumentParser(description='Separates a dataset of mel spectrograms using source separation')
+    parser.add_argument('--audio_dir',
                         help='Directory containing .wav files for training')
     parser.add_argument('--sep', type=str, required=True, choices=['hpss', 'cen'],
                         help='Separation method to use ("hpss" or "cen").')
     parser.add_argument('--pad', type=int, default=0,
                         help='Padding to add to the spectral centroid indices when using "cen" method.')
-    main(parser.parse_args())
+    parser.add_argument('--duration', type=float, default=1.0,
+                        help='Uniform duration for the audio files in seconds.')
+    parser.add_argument('--hop', type=float, default=0.5,
+                        help='Hop size in seconds for splitting audio files into shorter segments.')
+    parser.add_argument('--sr', type=int, default=22050,
+                        help='Target sample rate for the audio files.')
+    args = parser.parse_args()
+    
+    args.audio_dir = f'{args.audio_dir}_d{args.duration}_h{args.hop}_sr{args.sr}'
+    
+    main(args)
+
--- mvd/segment_audio.py
+++ mvd/segment_audio.py
@@ -1,42 +1,63 @@
 import os
-import torch
-import torchaudio as T
+import numpy as np
+import soundfile as sf
+import librosa
 
 from argparse import ArgumentParser
 from concurrent.futures import ProcessPoolExecutor
 from glob import glob
 from tqdm import tqdm
 
+def clean_filename(filename):
+    return filename.replace(' ', '_').replace('(', '').replace(')', '')
 
-def segment_audio(filename, output_dir, duration, hop):
-    audio, sr = T.load(filename)
-    audio = torch.clamp(audio[0], -1.0, 1.0)
+def segment_audio(filename, output_dir, duration, hop, sr_target):
+    audio, sr = sf.read(filename)
+
+    # Handle mono and stereo audio separately
+    if len(audio.shape) == 1:
+        # Mono audio
+        if sr != sr_target:
+            audio = librosa.resample(audio, sr, sr_target)
+            sr = sr_target
+    else:
+        # Stereo audio
+        if sr != sr_target:
+            audio = np.stack([librosa.resample(channel, sr, sr_target) for channel in audio.T])
+            sr = sr_target
 
     duration_samples = int(duration * sr)
     hop_samples = int(hop * sr)
 
+    # Calculate number of segments that will fit into the audio file
+    num_segments = 1 + (len(audio) - duration_samples) // hop_samples
+
     # Split the audio into shorter segments
-    for i in range(0, len(audio) - duration_samples, hop_samples):
-        segment = audio[i:i + duration_samples]
-        output_filename = os.path.join(output_dir, f'{os.path.splitext(os.path.basename(filename))[0]}_{i}.wav')
-        T.save(output_filename, segment, sr)
+    for i in range(num_segments):
+        start = i * hop_samples
+        segment = audio[start:start + duration_samples]
+        output_filename = os.path.join(output_dir, f'{clean_filename(os.path.splitext(os.path.basename(filename))[0])}_{i}.wav')
+        sf.write(output_filename, segment, sr)
 
 
 def main(args):
-    filenames = glob(f'{args.dir}/**/*.wav', recursive=True)
-    os.makedirs(args.output_dir, exist_ok=True)
-    with ProcessPoolExecutor() as executor:
-        list(tqdm(executor.map(lambda filename: segment_audio(filename, args.output_dir, args.duration, args.hop), filenames), desc='Segmenting', total=len(filenames)))
+    output_dir = f'../data/{args.audio_dir}_d{args.duration}_h{args.hop}_sr{args.sample_rate}'
+    os.makedirs(output_dir, exist_ok=True)
+
+    filenames = glob(f'../data/{args.audio_dir}/*.wav')
+
+    for filename in tqdm(filenames, desc='Segmenting', total=len(filenames)):
+        segment_audio(filename, output_dir, args.duration, args.hop, args.sample_rate)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Segments audio files into uniform duration')
-    parser.add_argument('--dir',
+    parser.add_argument('--audio_dir',
                         help='Directory containing .wav files for training')
-    parser.add_argument('--output_dir', type=str, default='uniform_duration',
-                        help='Output directory for segmented audio files.')
     parser.add_argument('--duration', type=float, default=1.0,
                         help='Uniform duration for the audio files in seconds.')
     parser.add_argument('--hop', type=float, default=0.5,
                         help='Hop size in seconds for splitting audio files into shorter segments.')
+    parser.add_argument('--sample_rate', type=int, default=22050,
+                        help='Target sample rate for audio files. Audio will be resampled if necessary.')
     main(parser.parse_args())
